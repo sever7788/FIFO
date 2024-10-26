db575a1a1bf93225e81e38e0d38b7a5aeda06a2b
sever7788 <star.traveler7788@gmail>
2024-10-26 19:54:51 +0300

    fix: added pointers

--- design.sv
+++ design.sv
@@ -9,30 +9,41 @@ module FIFO
    input logic [width-1:0] data,
    output logic full,
    output logic empty,
+   output logic [length-1:0] SRAM [width-1:0],
    output logic [width-1:0] out 
 );
-  logic [width-1:0][length-1:0] FIFO;
-  logic [$clog2(length-1)-1:0] pop_ptr;
-  logic [$clog2(length-1)-1:0] push_ptr;
+  `define max_ptr length
+  `define ptr_len $clog2(length) - 1
+  
+  //logic [length-1:0] SRAM [width-1:0];
+  logic [`ptr_len:0] pop_ptr;
+  logic [`ptr_len:0] push_ptr;
+  logic [`ptr_len + 1:0] cnt;
   
   always_ff @(posedge clk or negedge rstn) begin
     if(!rstn) begin
-      FIFO     <= 16'b0;
-      pop_ptr  <= '0;
-      push_ptr <= '0;
+      for(int i = 0; i < length; i++)
+        SRAM[i] <= '0;
+        cnt      <= '0;
+        pop_ptr  <= '0;
+        push_ptr <= '0;
     end else begin
       if(push && !full) begin
-        FIFO[push_ptr] <= data;
+        SRAM[push_ptr] <= data;
         push_ptr <= push_ptr + 1;
+        cnt <= cnt + 1;
       end
       if(pop && !empty) begin
-        FIFO[pop_ptr] <= '0;
         pop_ptr <= pop_ptr + 1;
-        out <= FIFO[pop_ptr];
+        cnt <= cnt - 1;
       end
     end
   end
-  assign full  = FIFO[push_ptr];
-  assign empty = !FIFO[pop_ptr];
+  
+  assign full  = cnt == `max_ptr;
+  assign empty = ~|cnt;
+  assign out = SRAM[pop_ptr];
+  //assign out = (empty || !pop) ? 'Z : SRAM[pop_ptr];
+  
     
 endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,4 +1,3 @@
-
 module test
   #(parameter width = 4, length = 4)
   (output logic clk,
@@ -8,7 +7,8 @@ module test
    output logic [width-1:0] data,
    input logic full,
    input logic empty,
-   input logic [width-1:0] out 
+   input logic [width-1:0] out,
+   input logic [length-1:0] SRAM [width-1:0]
 );
   timeunit 1ns/1ns;
   
@@ -24,12 +24,15 @@ module test
     push = 0;
     pop = 0;
     data = 0;
-	#15 rstn = 1;
+	  #10 rstn = 1;
     push = 1;
+    #5;
     for(int i = 0; i < 5; i++ ) begin
-      #15 data = i;
+      data = i;
+      #10;
     end
     push = 0;
+    #5;
     pop = 1;
     #100 $finish;
   end
